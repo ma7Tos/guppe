c9fa4df27200408685de07cd92005cc3c6867d58
Felipe Mattos <felipe_mtt@outlook.com>
2024-09-04 17:57:10 -0300

    Adicionando aulas da sessão 7.

---
+++ Aulas/funcoes_com_retorno.py
@@ -0,0 +1,19 @@
+"""
+Funções com retorno
+
+numeros = [1, 2, 3]
+
+ret_pop = numeros.pop()
+print(f'retorno de pop: {ret_pop}')
+
+ret_pr = print(numeros)
+print(f'retorno de print: {ret_pr}')
+
+"""
+
+#Exemplo função
+
+def quadrado_de_7():
+    print(7 * 7)
+
+quadrado_de_7()
