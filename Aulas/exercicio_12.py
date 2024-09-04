56e16384b8b8516b7d215759819c73aaf8152d21
Felipe Mattos <felipe_mtt@outlook.com>
2024-09-04 11:32:33 -0300

    Adicionando resolução dos exercícios da sessão 6.

--- Aulas/exercicio_12.py
+++ Aulas/exercicio_12.py
@@ -7,7 +7,7 @@ produto = ''
 
 print('Em nossa loja, a compra mínima é de 10 itens, não adianta chorar, ta duro vá pra casa!')
 while len(sacola) < 10:
-    print('Insira um produto no carrinho: ')
+    print(f'Insira o produto {len(sacola) + 1}/10 no carrinho: ')
     produto = input()
     sacola.append(produto)
     if len(sacola) == 10:
