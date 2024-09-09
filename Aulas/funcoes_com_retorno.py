b5d67f46080aca32c56302ce264375aed4c5ff10
Felipe Mattos <felipe_mtt@outlook.com>
2024-09-09 17:54:49 -0300

    Adicionando aulas da sessão 7.

--- Aulas/funcoes_com_retorno.py
+++ Aulas/funcoes_com_retorno.py
@@ -4,16 +4,75 @@ Funções com retorno
 numeros = [1, 2, 3]
 
 ret_pop = numeros.pop()
+
 print(f'retorno de pop: {ret_pop}')
 
 ret_pr = print(numeros)
+
 print(f'retorno de print: {ret_pr}')
 
-"""
 
-#Exemplo função
+OBS.: Em Python, quando uma função não retorna nenhum valor, o retorno é None.
+
+OBS.: Funções Python que retornam valores, devem retornar estes valores com a palavra reservada return
+
+OBS.: Não precisamos necessaruamente criar uma variável para receber o retorno de uma função. Podemos passar a
+execução da função para outras funções.
+
+#Vamos refatorar estafunção para que ela retorne o valor
 
 def quadrado_de_7():
-    print(7 * 7)
+    return 7 * 7
 
-quadrado_de_7()
+#Criamos uma variável para receber o retorno da função
+ret = quadrado_de_7()
+print(f"Retorno {ret}")
+
+print(f"Retorno: {quadrado_de_7()}")
+
+# Refatorando a primeira função
+
+def diz_oi():
+    return "Oi"
+
+alguem = "Pedro!"
+
+print(diz_oi())
+print(alguem)
+
+OBS.: Sobre a palavra reservada return
+
+1 - Ela finaliza a função, ou seja, ela sai da execução;
+2 - Podemos ter, em uma função, diferentes returns;
+3 - Podemos, em uma função, retornar qualquer tipo de dado e até mesmo múltiplos valores;
+
+# Exemplo 1
+
+def diz_oi():
+    print("Estou sendo executado antes do retorno...")
+    return "Oi"
+    print("Estou sendo executado após o retorno...")
+
+print(diz_oi())
+
+# Exemplo 2
+
+def nova_funcao():
+    variavel = False
+    if variavel:
+        return 4
+    elif variavel is None:
+        return 3.2
+    return "b"
+
+print(nova_funcao())
+"""
+
+# Exemplo 3
+
+def outra_funcao():
+    return 2, 3, 4, 5
+
+num1, num2, num3, num4 = outra_funcao()
+
+print(num1, num2, num3, num4)
